cc9846c8e347a529e2ca9aca46733e9fa0ad2c5a
djyos1 <57334875+djyos1@users.noreply.github.com>
2022-04-02 14:49:55 -0400

    greyBox testing testing  TC001

---
+++ src/TestCases/TC002.java
@@ -0,0 +1,16 @@
+package TestCases;
+
+public class TC002 {
+
+    public static void main(String[] args) {
+
+// test case 2
+        // creating smoke test
+
+        // screenshot in failure
+
+
+
+
+    }
+}
